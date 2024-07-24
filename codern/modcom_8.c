c122cf41f42225929570b5bab90261cb9abadbe7
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2024-07-24 23:07:17 +0700

    add tests, add context

--- codern/modcom_8.c
+++ codern/modcom_8.c
@@ -1,3 +1,18 @@
+/*
+You are given a series of equations in the form:
+A _ B _ C = ?
+
+There are two hidden operators (represented by _) that are consistent across all equations. Your task is to determine these operators and use them to calculate the result for each given set of A, B, and C values.
+
+Write a program that takes three integers as input (A, B, and C) and outputs the result of applying the hidden operations.
+
+Example case
+Input (A B C) 	Output
+240 4 3 	    180
+1000 5 7 	    1400
+360 3 2 	    240
+*/
+
 #include <stdio.h>
 
 int main(){
